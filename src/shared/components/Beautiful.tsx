187b2ad8c2899017883fe6ae3bfeae464e819416
boburmirzo-brm <boburmirzo.brm@gmail.com>
2025-07-28 17:13:57 +0500

    added button

--- src/shared/components/Beautiful.tsx
+++ src/shared/components/Beautiful.tsx
@@ -4,6 +4,7 @@ const Beautiful = () => {
     <div>
       <h2>Beautiful</h2>
       <p>Далеко-далеко, за словесными горами в стране гласных и согласных живут рыбные тексты.</p>
+      <button>See more</button>
     </div>
   )
 }
