a199d2282bf5581ae286d3f49611a5932f6e9f56
Muhammadumar-Umarov <surnameen22@gmail.com>
2025-07-28 17:02:46 +0500

    added Testimoniels

--- src/shared/components/Testimoniels.tsx
+++ src/shared/components/Testimoniels.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
 
 const Testimoniels = () => {
   return (
-    <div>Testimoniels</div>
+    <div>
+      <h1 style={{fontSize: 28}}>This is Muhammadumar's branch</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, omnis ea aliquid, assumenda illum facilis eligendi nemo tempora expedita culpa ratione magnam, odit earum. Id voluptatem dicta iure quia provident?</p>
+    </div>
   )
 }
 
